a0df4bdd409a78d3f5663e38028c2a1508e5a6ad
Ricardo <r2volk@gmail.com>
2026-05-02 03:55:57 -0500

    Avance fronted Angular

--- frontend/hotel-frontend/src/app/app.ts
+++ frontend/hotel-frontend/src/app/app.ts
@@ -1,29 +1,42 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core'; // necesario de Angular
+import { CommonModule } from '@angular/common'; // funcionalidades basicas (como ngFor)
 
+
+// Decorador que define el componente
 @Component({
-  selector: 'app-root',
-  imports: [CommonModule],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: 'app-root', // nombre del componente
+  imports: [CommonModule], // habilita ngFor
+  templateUrl: './app.html', // HTML que usa
+  styleUrl: './app.css' // estilos
 })
 export class App {
+
   habitaciones: any[] = [];
   clientes: any[] = [];
+  reservas: any[] = [];
 
-  cargarHabitaciones() {
-    fetch('http://localhost:8080/habitaciones')
-      .then(respuesta => respuesta.json())
-      .then(datos => {
-        this.habitaciones = datos;
-      });
+  async cargarHabitaciones() {
+    //pide al backend
+    const respuesta = await fetch('http://localhost:8080/habitaciones');
+    // convierte la respuesta a json
+    const datos = await respuesta.json();
+    //guarda los datos en la lista
+    this.habitaciones = datos;
   }
 
-  cargarClientes() {
-    fetch('http://localhost:8080/clientes')
-      .then(respuesta => respuesta.json())
-      .then(datos => {
-        this.clientes = datos;
-      });
+  // Funcion para cargar clientes
+  async cargarClientes() {
+    const respuesta = await fetch('http://localhost:8080/clientes');
+    const datos = await respuesta.json();
+    this.clientes = datos;
   }
+
+
+  // Funcion asincrona para cargar reservas
+  async cargarReservas() {
+    const respuesta = await fetch('http://localhost:8080/reservas');
+    const datos = await respuesta.json();
+    this.reservas = datos;
+  }
+
 }
